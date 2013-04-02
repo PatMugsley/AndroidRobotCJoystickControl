56b8a794e4eea4691f80b8fb5ad772252cf07781
Ken Pugsley <ken@pugsley-macmini.local>
2013-04-01 20:50:37 -0500

    Added base android app

---
+++ gen/org/kcastromechs/arcjoystickcontrol/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.kcastromechs.arcjoystickcontrol;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
