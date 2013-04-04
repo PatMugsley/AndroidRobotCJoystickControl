a46896417c3512dc6f907847a2b99398f8649231
jenkinear1 <ken@pugsley.net>
2013-04-03 19:42:12 -0500

    change to bt library

--- src/org/kcastromechs/arcjoystickcontrol/bt/NXTMessageUtil.java
+++ src/org/kcastromechs/arcjoystickcontrol/bt/NXTMessageUtil.java
@@ -14,7 +14,7 @@
 // TODO Reconstruct to support english decoding of messages (in and out)
 // TODO Add in methods to support message writing
 
-package org.kcastromechs.arcjoystickcontrol.util;
+package org.kcastromechs.arcjoystickcontrol.bt;
 
 public class NXTMessageUtil {
 
